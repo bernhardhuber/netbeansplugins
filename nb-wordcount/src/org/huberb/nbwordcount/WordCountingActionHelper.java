c941671dd9556422d0f5c9695455719480a2af18
berni3 <berni3@DESKTOP-GHTTUME>
2017-10-11 22:08:42 +0200

    refactor code

--- nb-wordcount/src/org/huberb/nbwordcount/WordCountingActionHelper.java
+++ nb-wordcount/src/org/huberb/nbwordcount/WordCountingActionHelper.java
@@ -1,9 +1,12 @@
 package org.huberb.nbwordcount;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.Predicate;
 import javax.swing.SwingUtilities;
 import org.huberb.nbwordcount.model.WordCountBean;
 import org.netbeans.api.progress.ProgressHandle;
-import org.netbeans.api.progress.ProgressHandleFactory;
 import org.openide.filesystems.FileObject;
 import org.openide.loaders.DataObject;
 import org.openide.nodes.Node;
@@ -11,80 +14,84 @@ import org.openide.util.Cancellable;
 import org.openide.util.NbBundle;
 
 /**
- * This class implements the task of examing the
- * counter information asynchrounsly.
+ * This class implements the task examining the counter information
+ * asynchronously.
  */
 class WordCountingActionHelper implements Cancellable {
+
     private boolean isCancelled = false;
     private final WordCountBean wcb;
     private final ProgressHandle ph;
-    
-    /** Creates a new instance of WordCountBean */
+
+    /**
+     * Creates a new instance of WordCountBean
+     */
     public WordCountingActionHelper() {
         this.isCancelled = false;
         // create the bean holding the counting info
         this.wcb = new WordCountBean();
-        
+
         // create the ProgessHandle
         final String phDisplayName = NbBundle.getMessage(WordCountingAction.class, "PRG_WordCountingAction");
-        this.ph = ProgressHandleFactory.createHandle( phDisplayName, this );
+        this.ph = ProgressHandle.createHandle(phDisplayName, this);
     }
-    
+
     // Implementation of Cancellable
     public boolean cancel() {
-        this.isCancelled  = true;
+        this.isCancelled = true;
         return this.isCancelled;
     }
 
     /**
-     * Use this method for creating a <code>Runnable</code> used for
-     * gathering counter information, asynchronously.
+     * Use this method for creating a <code>Runnable</code> used for gathering
+     * counter information, asynchronously.
      *
-     * @return Runnable exmining the counter information, and
-     *   updating the UI
+     * @return Runnable examining the counter information, and updating the UI
      */
     public Runnable createTask(final Node[] activatedNodes) {
         // reset cancel, the previous action may have been cancelled
         this.isCancelled = false;
-        
+
         // create a Runnable presenting the result in the Swing's EventDispatchThread
         final Runnable viewResult = new Runnable() {
+            @Override
             public void run() {
                 // get name, and values of the counters
                 final Long[] counters = wcb.getCounters();
                 final String namesOfCounters = wcb.getNamesOfFiles();
                 final int countOfFiles = wcb.getCountOfFiles();
-                
+
                 // Update the WordCount TopComponent window
                 final WordCountTopComponent win = WordCountTopComponent.findInstance();
                 if (win != null) {
-                    win.setNamesOfCounters( countOfFiles, namesOfCounters );
-                    win.setCounters( counters );
+                    win.setNamesOfCounters(countOfFiles, namesOfCounters);
+                    win.setCounters(counters);
                     win.requestActive();
                 }
             }
         };
-        
+
         // create a Runnable not running in the Swing EventDispatchThread
         final Runnable task = new Runnable() {
+            @Override
             public void run() {
                 try {
                     // start the progress
                     ph.start();
                     // calculate the count info
-                    countAllNodes( activatedNodes );
+                    countAllNodes(activatedNodes);
                 } finally {
                     // finish the progress
                     ph.finish();
                     // present the result in Swing's EventDispathThread
-                    SwingUtilities.invokeLater( viewResult );
+                    SwingUtilities.invokeLater(viewResult);
                 }
             }
         };
-        
+
         return task;
     }
-    
+
     /**
      * Iterate over all activedNodes object.
      *
@@ -95,17 +102,17 @@ class WordCountingActionHelper implements Cancellable {
         for (int i = 0; !this.isCancelled && i < activatedNodes.length; i++) {
             final DataObject dataObject = (DataObject) activatedNodes[i].getCookie(DataObject.class);
             final FileObject fileObject = dataObject.getPrimaryFile();
-            
-            count( fileObject );
+
+            count(fileObject);
         }
     }
-    
+
     /**
      * Calculate counts for a FileObject.
      * <p>
-     * Either calculate the counts if the FileObject is real file, or
-     * if the FileObject is a directory, recursivly calculate the counts for
-     * all children od this directory.
+     * Either calculate the counts if the FileObject is real file, or if the
+     * FileObject is a directory, recursively calculate the counts for all
+     * children of this directory.
      *
      * @param fo the FileObject for which the count info is calculated
      */
@@ -113,39 +120,49 @@ class WordCountingActionHelper implements Cancellable {
         if (fo.isFolder()) {
             final boolean folderIsOkay = isFolderOkay(fo);
             if (!folderIsOkay) {
-                return ;
+                return;
             }
             final FileObject[] foChildren = fo.getChildren();
             for (int i = 0; !this.isCancelled && i < foChildren.length; i++) {
-                count( foChildren[i] );
+                count(foChildren[i]);
             }
-        }  else if (fo.isData()) {
+        } else if (fo.isData()) {
             final boolean fileIsOkay = isFileOkay(fo);
             if (!fileIsOkay) {
-                return ;
+                return;
             }
-            
+
             // update the progressHandle
             final String foProgress = fo.getNameExt();
-            ph.progress( foProgress );
-            
+            ph.progress(foProgress);
+
             wcb.setFileObject(fo);
             wcb.count();
         }
     }
-    
+
     /**
      * Check if folder is ok for counting
+     *
      * @param foFolder the folder file object
      * @return boolean true if we shall count files of this folder
      */
     protected boolean isFolderOkay(FileObject foFolder) {
         boolean folderIsOkay = foFolder.canRead();
-        folderIsOkay = folderIsOkay  && !foFolder.getName().toLowerCase().equals("cvs");
+        Set<String> ignoreDirSet = new HashSet<>(
+                Arrays.asList("cvs", ".svn", ".git")
+        );
+        final Predicate<String> containedInIgnoreDirSet = (String dirName) -> {
+            final String lowerCaseDirName = dirName.toLowerCase();
+            return ignoreDirSet.contains(lowerCaseDirName);
+        };
+        folderIsOkay = folderIsOkay && !containedInIgnoreDirSet.test(foFolder.getName());
         return folderIsOkay;
     }
+
     /**
      * Check if file is ok for counting
+     *
      * @param foFile the file object
      * @return boolean true if we shall examine this file for counting
      */
--- nb-wordcount/src/org/huberb/nbwordcount/model/WordCountBean.java
+++ nb-wordcount/src/org/huberb/nbwordcount/model/WordCountBean.java
@@ -6,17 +6,16 @@
  * To change this template, choose Tools | Template Manager
  * and open the template in the editor.
  */
-
 package org.huberb.nbwordcount.model;
 
 import java.io.BufferedReader;
-import java.io.Closeable;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Iterator;
+
 import java.util.List;
 
 import org.huberb.nbwordcount.model.counters.JavaStatementLineCounter;
@@ -30,71 +29,71 @@ import org.openide.filesystems.FileObject;
  * @author HuberB1
  */
 public class WordCountBean implements Serializable {
+
     protected final static long serialVersionUID = 20060124151400L;
 
     private FileObject fo;
-    private List<String> foNames;
-    private SimpleLineCounter simpleLineCounter;
-    private JavaStatementLineCounter javaStatementLineCounter;
-    
-    /** Creates a new instance of WordCountBean */
+    private final List<String> foNames;
+    private final SimpleLineCounter simpleLineCounter;
+    private final JavaStatementLineCounter javaStatementLineCounter;
+
+    /**
+     * Creates a new instance of WordCountBean
+     */
     public WordCountBean() {
         this.fo = null;
-        this.foNames = new ArrayList<String>();
+        this.foNames = new ArrayList<>();
         this.simpleLineCounter = new SimpleLineCounter();
         this.javaStatementLineCounter = new JavaStatementLineCounter();
     }
-    
+
     /**
      * Set the FileObject used for calculating the counter informations
+     *
      * @param fo the FileObject used for calculating the counter informations
      */
-    public void setFileObject( FileObject fo ) {
+    public void setFileObject(FileObject fo) {
         this.fo = fo;
     }
-    
+
     /**
      * Calculate the counter information.
      * <p>
-     * This is the main entry point of this class for gathering
-     * count information of the currently set file
+     * This is the main entry point of this class for gathering count
+     * information of the currently set file
      * <p>
      * FileObject's InputStream is read to calculate the counting information
      */
     public void count() {
-        InputStream is = null;
-        try {
-            is = this.fo.getInputStream();
+
+        try (InputStream is = this.fo.getInputStream()) {
             count(is);
-            
+
             final String foName = fo.getNameExt();
-            foNames.add( foName );
+            foNames.add(foName);
         } catch (IOException ioex) {
             // ignore it
-        } finally {
-            closeSilently(is);
         }
     }
 
     /**
-     * Get the names of all FileObject's we have processed, as 
-     * CSV String
+     * Get the names of all FileObject's we have processed, as CSV String
      *
      * @return String a CSV String of the FileObject's names
      */
     public String getNamesOfFiles() {
         final StringBuilder sb = new StringBuilder();
-        
-        for (Iterator<String> i = this.foNames.iterator(); i.hasNext(); ) {
+
+        for (Iterator<String> i = this.foNames.iterator(); i.hasNext();) {
             String foName = i.next();
-            sb.append( foName );
+            sb.append(foName);
             if (i.hasNext()) {
-                sb.append( ", " );
+                sb.append(", ");
             }
         }
         return sb.toString();
     }
-    
+
     /**
      * Get the count of files examined
      *
@@ -103,63 +102,48 @@ public class WordCountBean implements Serializable {
     public int getCountOfFiles() {
         return this.foNames.size();
     }
-    
+
     /**
      * Get the counters.
-     * @return Long[] of counter values, 
-     * in the following sequence
+     *
+     * @return Long[] of counter values, in the following sequence
      * <ul>
-     *   <li>Count of characters
-     *   <li>Count of words
-     *   <li>Count of lines
+     * <li>Count of characters
+     * <li>Count of words
+     * <li>Count of lines
      * </ul>
      */
     public Long[] getCounters() {
-        CounterInfo ci1 = this.simpleLineCounter.getCounterInfo();
-        CounterInfo ci2 = this.javaStatementLineCounter.getCounterInfo();
-        Number[] counters1 = ci1.getCounterValues();
-        Number[] counters2 = ci2.getCounterValues();
-        
-        final Long[] counterValues = new Long[] {
-            Long.valueOf(counters1[0].longValue() ),
-            Long.valueOf( counters1[1].longValue() ),
-            Long.valueOf( counters1[2].longValue() ),
-            Long.valueOf( counters2[0].longValue() ),
-            Long.valueOf( counters2[1].longValue() ),
-            Long.valueOf( counters2[2].longValue() ),
-        };
+        CounterInfo simpleCounterInfo = this.simpleLineCounter.getCounterInfo();
+        CounterInfo javaStatementCounterInfo = this.javaStatementLineCounter.getCounterInfo();
+        Number[] simpleCounterValues = simpleCounterInfo.getCounterValues();
+        Number[] javaStatementValues = javaStatementCounterInfo.getCounterValues();
+
+        final Long[] counterValues = new Long[]{
+            simpleCounterValues[0].longValue(),
+            simpleCounterValues[1].longValue(),
+            simpleCounterValues[2].longValue(),
+            javaStatementValues[0].longValue(),
+            javaStatementValues[1].longValue(),
+            javaStatementValues[2].longValue(),};
         return counterValues;
     }
-    
+
     /**
      * Examine each line of this stream.
      *
      * @param is lines of this input stream are examined
      */
     private void count(InputStream is) throws IOException {
-        BufferedReader in = null;
-        
-        try {
+
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(is))) {
+
             String line;
-            in = new BufferedReader(new InputStreamReader(is));
-            
             while ((line = in.readLine()) != null) {
-                this.simpleLineCounter.count( line );
-                this.javaStatementLineCounter.count( line );
-            }
-        } finally {
-            closeSilently(in);
-        }
-    }
-    
-    private void closeSilently(Closeable is) {
-        if (is != null) {
-            try {
-                is.close();
-            } catch (IOException ioex) {
-                // ignore it
+                this.simpleLineCounter.count(line);
+                this.javaStatementLineCounter.count(line);
             }
         }
     }
-    
+
 }
